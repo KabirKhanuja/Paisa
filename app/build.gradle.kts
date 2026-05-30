66329b8e0b7df61976f31d628cae912c91b92c16
KabirKhanuja <kabirkhanuja@gmail.com>
2026-05-31 02:25:54 +0530

    setting compile and target sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "kabir.paisa"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "kabir.paisa"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
 
